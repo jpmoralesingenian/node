c932f1f99835cfa1df115bc5e5bd967f4a2f42a0
Juan Pablo Morales <jpmorales@ingenian.com>
2015-11-21 07:30:06 -0500

    First version of the meseros crud table

--- resto/app.js
+++ resto/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var meseros = require('./routes/meseros');
 var app = express();
-app.get('/api/meseros', function(req, res) {
-    res.send([{id:1,name:'Homero'}, {id:2,name:'Moe'}, {id:3,name:'Horace'}, {id:4,name:'Philip'}, {id:5,name:'Bender'}]);
-});
-app.get('/api/meseros/:id', function(req, res) {
-    res.send({id:req.params.id, name: "The Name"});
-});
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.get('/api/meseros', meseros.findAll );
+app.get('/api/meseros/:id', meseros.findById );
+app.post('/api/meseros', meseros.addMesero);
+app.put('/api/meseros/:id', meseros.updateMesero);
+app.delete('/api/meseros/:id', meseros.deleteMesero);
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
--- resto/routes/meseros.js
+++ resto/routes/meseros.js
@@ -1,6 +1,104 @@
-exports.findAll = function(req,res) {
-	res.send([{id:1,name:'Homero'}, {id:2,name:'Moe'}, {id:3,name:'Horace'}, {id:4,name:'Philip'}, {id:5,name:'Bender'}]);
+var mongo = require('mongodb');
+var util = require('util');
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('resto', server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'resto' database");
+        db.collection('meseros', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'meseros' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving mesero: ' + id);
+    db.collection('meseros', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
 };
-exports.findById = function(req,res) {
-    res.send({id:req.params.id, name: "The Name"});
+
+exports.findAll = function(req, res) {
+    db.collection('meseros', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.addMesero = function(req, res) {
+    console.log('Request is: ' + util.inspect(req.body));
+    var mesero = req.body;
+    console.log('Adding mesero: ' + JSON.stringify(mesero));
+    db.collection('meseros', function(err, collection) {
+        collection.insert(mesero, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+exports.updateMesero = function(req, res) {
+    var id = req.params.id;
+    var mesero = req.body;
+    console.log('Updating mesero: ' + id);
+    console.log(JSON.stringify(mesero));
+    db.collection('meseros', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, mesero, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating mesero: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(mesero);
+            }
+        });
+    });
+}
+
+exports.deleteMesero = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting mesero: ' + id);
+    db.collection('meseros', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+
+    var meseros = [
+    { name: "Homero"},
+    { name: "Moe"},
+    { name: "Horace"},
+    { name: "Philip"},
+    { name: "Bender"}];
+
+    db.collection('meseros', function(err, collection) {
+        collection.insert(meseros, {safe:true}, function(err, result) {});
+    });
+
 };
